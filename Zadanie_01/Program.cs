6498937d76c60ac093e2c65ace2e7247a946d8df
Eduard <eduard.sher1@gmail.com>
2023-01-27 16:31:22 +0300

    Zadanie_01 Найти кубы чисел от 1 до N

---
+++ Zadanie_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Write("Введите число от 1, до которого надо возводить в кубы: ");
+int N = int.Parse(Console.ReadLine()!);  //Число на котором остановится возведение в куб
+int number = 1;
+
+while (number <= N)
+{
+    Console.WriteLine(number * number * number);
+    number++;
+}
+
