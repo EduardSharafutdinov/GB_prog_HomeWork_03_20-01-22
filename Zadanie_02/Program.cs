1e899865ed45ea452aa72c1fcff7d8a4358a6e33
Eduard <eduard.sher1@gmail.com>
2023-01-27 19:49:37 +0300

    Zadanie_02 Подсчитать сумму цифр в числе

--- Zadanie_02/Program.cs
+++ Zadanie_02/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Подсчитать сумму цифр в числе
 
+Console.Write("Введите число у которого надо подсчитать сумму цифр: ");
+int N = int.Parse(Console.ReadLine()!); 
+int sum = 0;
+
+while (N != 0)
+{
+    sum += N % 10;
+    N / 10;   
+}
+
+Console.WriteLine(sum);
