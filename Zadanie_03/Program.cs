3da359acdc8ca71705396295e8ba01d521827795
Eduard <eduard.sher1@gmail.com>
2023-01-31 00:03:00 +0300

    Zadanie_03 Написать программу вычисления произведения чисел от 1 до N

--- Zadanie_03/Program.cs
+++ Zadanie_03/Program.cs
@@ -1,1 +1,13 @@
 ﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число до которой надо вычислить произведения: ");
+int N = int.Parse(Console.ReadLine()!);
+int number =1;
+int p=1;
+
+for (int i=number; i<=N; i++)
+{
+    p=p*i;
+    Console.WriteLine(p);
+}
+
