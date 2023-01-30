810678ca41e0dd2cfe34b5186edd1e9bff6d5c6b
Eduard <eduard.sher1@gmail.com>
2023-01-30 23:43:37 +0300

    Zadanie_02  Показать кубы чисел, заканчивающихся на четную цифру

--- Zadanie_04/Program.cs
+++ Zadanie_04/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите число до которой будут возводиться в кубы: ");
+int N = int.Parse(Console.ReadLine()!);
+int number = 1;
+int stepen = 3;
+int kub = 1;
+
+while (number <= N)
+{
+    kub = Convert.ToInt32(Math.Pow(number, stepen));
+    if (kub%2==0) Console.WriteLine(kub);
+    number++;
+}
